2f1474aefb53dba54783d2cd19dd105eb5ff8ad7
Dmitry Spiridonov <57397762+dimacodeit@users.noreply.github.com>
2022-05-09 16:33:58 +0300

    Create react app (#2) (#3)
    * gitignore
    
    * #1 create react app
    
    * install sass
    
    * test routes
    
    * add formil and plain form

---
+++ src/App.tsx
@@ -0,0 +1,102 @@
+import logo from './logo.svg';
+import './App.scss';
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import { useFormik } from 'formik';
+import { useState } from 'react';
+
+function App() {
+  const [sub, setSub] = useState(false);
+
+  const submitHandle = () => setSub(true);
+
+  if (sub)
+    return (
+      <>
+        <nav className="plain-nav">
+          <Link to="/">Home</Link>
+          <Link to="about">About</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/about/*" element={<About />} />
+        </Routes>
+      </>
+    );
+
+  return <SignupForm submitted={submitHandle} />;
+}
+
+function SignupForm(props: any) {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      props.submitted();
+    },
+  });
+  return (
+    <div className="form-wrapper">
+      <form className="form" onSubmit={formik.handleSubmit}>
+        <label htmlFor="email">Email Address</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+        />
+
+        <button type="submit">Log in</button>
+      </form>
+    </div>
+  );
+}
+
+function MainPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <h1>About page</h1>
+      <Link to="nested">go nest</Link>
+      <Routes>
+        <Route path="/nested" element={<AboutNest />} />
+      </Routes>
+      <div className="outlet-nest">
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+function AboutNest() {
+  return (
+    <>
+      <p>Nested route</p>
+      <Link to="..">get back</Link>
+    </>
+  );
+}
+
+export default App;
