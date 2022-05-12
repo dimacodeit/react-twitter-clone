1c7a093098ebaea5c32c12d45e55309ea965f561
dmitry <dmitryspiridonov56@gmail.com>
2022-05-12 23:52:20 +0300

    redux logout

--- src/components/views/Views.tsx
+++ src/components/views/Views.tsx
@@ -1,6 +1,9 @@
 import { Link, Outlet, Route, Routes } from 'react-router-dom';
 import { SignupForm } from '../login/Login';
 import logo from '../../logo.svg';
+import { Button } from '@mui/material';
+import { useAppDispatch } from '../../hooks/redux';
+import { authSlice } from '../../store/reducers/AuthSlice';
 
 export interface IViewsProps {
   submitHandle: Function;
@@ -19,11 +22,26 @@ export function Views(props: IViewsProps) {
 }
 
 function MainPageView() {
+  const dispatch = useAppDispatch();
+  const { logout } = authSlice.actions;
+  // eslint-disable-next-line no-restricted-globals
+  const logoutHandler = () => confirm('Are you sure?') && dispatch(logout());
   return (
     <div className="App">
-      <nav className="plain-nav">
+      <nav className="plain-nav" style={{ position: 'relative' }}>
         <Link to="/">Home</Link>
         <Link to="about">About</Link>
+        <Button
+          style={{
+            position: 'absolute',
+            top: '50%',
+            right: '0',
+            transform: 'translateY(-50%)',
+          }}
+          onClick={logoutHandler}
+        >
+          logout
+        </Button>
       </nav>
       <Routes>
         <Route path="/about/*" element={<About />} />
