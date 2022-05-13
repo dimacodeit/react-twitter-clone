35bc4b0224d5336e1f268df3373865cd50565e15
dmitry <dmitryspiridonov56@gmail.com>
2022-05-13 20:43:56 +0300

    add icons

--- src/components/main-layout/nav-button/Nav-button.tsx
+++ src/components/main-layout/nav-button/Nav-button.tsx
@@ -1,8 +1,9 @@
+import { SvgIconComponent } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import styles from './Nav-button.module.scss';
 
 export interface INavButtonProps {
-  //   icon: string;
+  icon: SvgIconComponent;
   title: string;
   to: string;
 }
@@ -10,7 +11,16 @@ export interface INavButtonProps {
 export default function NavButton(props: INavButtonProps) {
   return (
     <Link tabIndex={-1} className={styles.link} to={props.to}>
-      <button className={styles.button}>{props.title}</button>
+      <button className={styles.button}>
+        <props.icon
+          className={styles.button__icon}
+          style={{
+            width: '35px',
+            height: '35px',
+          }}
+        />
+        {props.title}
+      </button>
     </Link>
   );
 }
--- src/components/main-layout/side-menu/Side-menu.tsx
+++ src/components/main-layout/side-menu/Side-menu.tsx
@@ -1,12 +1,17 @@
+import { HomeOutlined } from '@mui/icons-material';
+import TagOutlinedIcon from '@mui/icons-material/TagOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+
 import { Button } from '@mui/material';
 import { useAppDispatch } from '../../../hooks/redux';
 import { authSlice } from '../../../store/reducers/AuthSlice';
 import NavButton from '../nav-button/Nav-button';
 import styles from './Side-menu.module.scss';
 
-export interface ISideMenuProps {}
-
-export default function SideMenu(props: ISideMenuProps) {
+export default function SideMenu() {
   const dispatch = useAppDispatch();
   const { logout } = authSlice.actions;
   // eslint-disable-next-line no-restricted-globals
@@ -14,12 +19,20 @@ export default function SideMenu(props: ISideMenuProps) {
   return (
     <header className={styles.header}>
       <nav className={styles.nav} style={{ position: 'relative' }}>
-        <NavButton to="home" title="Home" />
-        <NavButton to="explore" title="Explore" />
-        <NavButton to="notifications" title="Notifications" />
-        <NavButton to="messages" title="Messages" />
-        <NavButton to="bookmarks" title="Bookmarks" />
-        <NavButton to="profile" title="Profile" />
+        <NavButton icon={HomeOutlined} to="home" title="Home" />
+        <NavButton icon={TagOutlinedIcon} to="explore" title="Explore" />
+        <NavButton
+          icon={NotificationsNoneOutlinedIcon}
+          to="notifications"
+          title="Notifications"
+        />
+        <NavButton icon={EmailOutlinedIcon} to="messages" title="Messages" />
+        <NavButton
+          icon={BookmarkBorderOutlinedIcon}
+          to="bookmarks"
+          title="Bookmarks"
+        />
+        <NavButton icon={PersonOutlinedIcon} to="profile" title="Profile" />
         <Button onClick={logoutHandler}>logout</Button>
       </nav>
     </header>
