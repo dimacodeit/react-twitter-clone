9d08081e6b62187bdcb7d4b233d53f18facdd22e
dmitry <dmitryspiridonov56@gmail.com>
2022-05-16 14:50:04 +0300

    trends comp & layouting

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,3 +1,4 @@
+import Trends from '../trends/Trends';
 import Card from './card/Card';
 import { MOCK_TWEETS } from './mocks';
 
@@ -5,10 +6,15 @@ export default function Home() {
   const mocks = MOCK_TWEETS;
   return (
     <div>
-      <h2 className="page-title">Home</h2>
-      {mocks.map((tweet, index) => (
-        <Card tweet={tweet} key={tweet.id} isFirst={index === 0} />
-      ))}
+      <div className="page-layout">
+        <div>
+          <h2 className="page-title">Home</h2>
+          {mocks.map((tweet, index) => (
+            <Card tweet={tweet} key={tweet.id} isFirst={index === 0} />
+          ))}
+        </div>
+        <Trends />
+      </div>
     </div>
   );
 }
---
+++ src/components/trends/Trends.tsx
@@ -0,0 +1,9 @@
+import styles from './Trends.module.scss';
+
+export default function Trends() {
+  return (
+    <div className={styles.trends}>
+      <h2>Trends for you</h2>
+    </div>
+  );
+}
