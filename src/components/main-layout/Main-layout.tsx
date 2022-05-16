99898d128206de897b7f47ab7de42eee0055ca31
Dmitry Spiridonov <57397762+dimacodeit@users.noreply.github.com>
2022-05-16 13:20:13 +0300

    Create components (#32)
    * [fix] logo error
    
    * [fix] manifest
    
    * route components

--- src/components/main-layout/Main-layout.tsx
+++ src/components/main-layout/Main-layout.tsx
@@ -1,4 +1,10 @@
-import { Link, Outlet, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Bookmarks from '../bookmarks/Bookmarks';
+import Explore from '../explore/Explore';
+import Home from '../home/Home';
+import Messages from '../messages/Messages';
+import Notifications from '../notifications/Notifications';
+import Profile from '../profile/Profile';
 import styles from './Main-layout.module.scss';
 import SideMenu from './side-menu/Side-menu';
 
@@ -8,42 +14,14 @@ export default function Layout() {
       <SideMenu />
       <main>
         <Routes>
-          <Route
-            path="home"
-            element={
-              <div>
-                <h1>Home</h1>
-                Илюх доверстай
-              </div>
-            }
-          />
-          <Route path="about/*" element={<About />} />
+          <Route path="home" element={<Home />} />
+          <Route path="explore" element={<Explore />} />
+          <Route path="notifications" element={<Notifications />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="bookmarks" element={<Bookmarks />} />
+          <Route path="profile" element={<Profile />} />
         </Routes>
       </main>
     </div>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <h1>About page</h1>
-      <Link to="nested">go nest</Link>
-      <Routes>
-        <Route path="/nested" element={<AboutNest />} />
-      </Routes>
-      <div className="outlet-nest">
-        <Outlet />
-      </div>
-    </>
-  );
-}
-
-function AboutNest() {
-  return (
-    <>
-      <p>Nested route</p>
-      <Link to="..">get back</Link>
-    </>
   );
 }
---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,5 @@
+export interface IProfileProps {}
+
+export default function Profile(props: IProfileProps) {
+  return <h1>Profile</h1>;
+}
