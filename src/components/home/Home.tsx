c24af324f025625d8bcea29b2e0456262dbe6441
dmitry <dmitryspiridonov56@gmail.com>
2022-05-16 21:24:25 +0300

    mock tweet

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,20 +1,30 @@
+import { useState } from 'react';
 import Trends from '../trends/Trends';
 import Card from './card/Card';
+import HomeHeader from './home-header/Home-header';
 import { MOCK_TWEETS } from './mocks';
 
 export default function Home() {
-  const mocks = MOCK_TWEETS;
+  const [mocks, setMocks] = useState([...MOCK_TWEETS]);
+  const tweetHandler = (text: string) => {
+    const newMock = [...mocks];
+    newMock.unshift({
+      name: 'Tweet',
+      text,
+      date: new Date().toString(),
+      id: Math.random(),
+    });
+    setMocks(newMock);
+  };
   return (
-    <div>
-      <div className="page-layout">
-        <div>
-          <h2 className="page-title">Home</h2>
-          {mocks.map((tweet, index) => (
-            <Card tweet={tweet} key={tweet.id} isFirst={index === 0} />
-          ))}
-        </div>
-        <Trends />
+    <div className="page-layout">
+      <div>
+        <HomeHeader tweetHandler={tweetHandler} />
+        {mocks.map((tweet, index) => (
+          <Card tweet={tweet} key={tweet.id} isFirst={index === 0} />
+        ))}
       </div>
+      <Trends />
     </div>
   );
 }
