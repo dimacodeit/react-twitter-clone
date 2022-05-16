9b0739ff7420ac96c3afe9b63e0ab281233449b0
dmitry <dmitryspiridonov56@gmail.com>
2022-05-16 13:03:24 +0300

    [fix] logo error

--- src/components/main-layout/Main-layout.tsx
+++ src/components/main-layout/Main-layout.tsx
@@ -1,4 +1,5 @@
-import { Link, Outlet, Route, Routes } from 'react-router-dom';
+import { Explore } from '@mui/icons-material';
+import { Route, Routes } from 'react-router-dom';
 import styles from './Main-layout.module.scss';
 import SideMenu from './side-menu/Side-menu';
 
@@ -17,33 +18,9 @@ export default function Layout() {
               </div>
             }
           />
-          <Route path="about/*" element={<About />} />
+          <Route path="explore/*" element={<Explore />} />
         </Routes>
       </main>
     </div>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <h1>About page</h1>
-      <Link to="nested">go nest</Link>
-      <Routes>
-        <Route path="/nested" element={<AboutNest />} />
-      </Routes>
-      <div className="outlet-nest">
-        <Outlet />
-      </div>
-    </>
-  );
-}
-
-function AboutNest() {
-  return (
-    <>
-      <p>Nested route</p>
-      <Link to="..">get back</Link>
-    </>
   );
 }
