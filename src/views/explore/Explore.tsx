9f339f8794c0ad878e4027e4de51cb6cb73ba5fc
dmitry <dmitryspiridonov56@gmail.com>
2022-05-20 00:04:25 +0300

    test request

--- src/views/explore/Explore.tsx
+++ src/views/explore/Explore.tsx
@@ -1,5 +1,16 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 export interface IExploreProps {}
 
 export default function Explore(props: IExploreProps) {
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`
+      )
+      .then((res) => console.log(res));
+  }, []);
+
   return <h1>Explore</h1>;
 }
