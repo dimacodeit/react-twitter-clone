503770ef1d0034e296ff8af0e7ca1342d418f85c
dmitry <dmitryspiridonov56@gmail.com>
2022-05-26 17:19:29 +0300

    confirm dialog

--- src/views/main/layout/side-menu/Side-menu.tsx
+++ src/views/main/layout/side-menu/Side-menu.tsx
@@ -3,25 +3,33 @@ import { useAppDispatch } from '@Hooks/redux';
 import { authSlice } from '@Store/reducers/AuthSlice';
 import Navigation from '../navigation/Navigation';
 import styles from './Side-menu.module.scss';
+import ConfirmDialog from '@Components/confirm-dialog/Confirm-dialog';
+import { useState } from 'react';
 
 export default function SideMenu() {
+  const [isOpen, setOpen] = useState(false);
   const dispatch = useAppDispatch();
   const { signOut } = authSlice.actions;
-  const logoutHandler = () => {
-    // eslint-disable-next-line no-restricted-globals
-    if (confirm('Are you sure?')) {
-      dispatch(signOut());
-      localStorage.removeItem('authName');
-    }
+
+  const confirmLogout = () => {
+    dispatch(signOut());
+    localStorage.removeItem('authName');
   };
+
   return (
     <div className={styles.menu}>
       <header className={styles.header}>
         <nav className={styles.nav} style={{ position: 'relative' }}>
           <Navigation />
-          <Button onClick={logoutHandler}>logout</Button>
+          <Button onClick={() => setOpen(true)}>logout</Button>
         </nav>
       </header>
+      <ConfirmDialog
+        titleText="Are you sure?"
+        isOpen={isOpen}
+        onConfirm={confirmLogout}
+        onReject={() => setOpen(false)}
+      />
     </div>
   );
 }
