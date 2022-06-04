300af74b3a7a18ea7d87c89f2712792fb95fa47f
dmitry <dmitryspiridonov56@gmail.com>
2022-06-04 20:03:32 +0300

    refactor home header component

--- src/views/home/home-header/Home-header.tsx
+++ src/views/home/home-header/Home-header.tsx
@@ -1,11 +1,5 @@
-import { Button } from '@mui/material';
-import React, {
-  FunctionComponent,
-  SyntheticEvent,
-  useRef,
-  useState,
-} from 'react';
-import styles from './Home-header.module.scss';
+import { FunctionComponent } from 'react';
+import TweetInput from '@Components/tweet-input/Tweet-input';
 
 interface HomeHeaderProps {
   tweetHandler: Function;
@@ -13,60 +7,6 @@ interface HomeHeaderProps {
 
 const HomeHeader: FunctionComponent<HomeHeaderProps> = (
   props: HomeHeaderProps
-) => {
-  const [textLength, setTextLength] = useState(0);
-  const textRef = useRef() as React.MutableRefObject<HTMLTextAreaElement>;
-
-  const onChangeHandler = (e: SyntheticEvent) => {
-    const target = e.target as HTMLTextAreaElement;
-    textRef.current.style.height = '30px';
-    textRef.current.style.height = `${target.scrollHeight}px`;
-    setTextLength(target.value.length);
-  };
-
-  const tweet = () => {
-    props.tweetHandler(textRef.current.value);
-    textRef.current.value = '';
-    textRef.current.style.height = '30px';
-    setTextLength(0);
-  };
-
-  const enterTweet = (e: React.KeyboardEvent) => {
-    if (e.ctrlKey && e.code === 'Enter') {
-      if (textLength <= 280) tweet();
-    }
-  };
-
-  return (
-    <div className={styles.header}>
-      <textarea
-        placeholder={`What's happening?`}
-        className={styles.header__textarea}
-        onChange={onChangeHandler}
-        onKeyDown={enterTweet}
-        ref={textRef}
-      ></textarea>
-
-      <div className={styles.button__container}>
-        <Button
-          variant="contained"
-          onClick={tweet}
-          disabled={!textLength || textLength > 280}
-        >
-          Tweet
-        </Button>
-        {textLength >= 240 && (
-          <div
-            className={`${styles.header__symbols} ${
-              textLength > 280 ? styles.red__label : ''
-            }`}
-          >
-            {textLength}
-          </div>
-        )}
-      </div>
-    </div>
-  );
-};
+) => <TweetInput tweetHandler={props.tweetHandler} />;
 
 export default HomeHeader;
