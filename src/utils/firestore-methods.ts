67d93b207024dd97404ec06fa4c15ae623f5ef89
dmitry <dmitryspiridonov56@gmail.com>
2022-07-11 17:22:51 +0300

    add delete method to firestore util

--- src/utils/firestore-methods.ts
+++ src/utils/firestore-methods.ts
@@ -2,9 +2,11 @@ import { Identifiable } from '@Models/identifiable';
 import {
   addDoc,
   collection,
+  deleteDoc,
   getDocs,
   orderBy,
   query,
+  doc,
 } from 'firebase/firestore';
 import { db } from '../firebase-config';
 
@@ -24,4 +26,9 @@ export async function getData<T extends Identifiable>(collectionName: string) {
 export async function addData<T>(collectionName: string, data: T) {
   const colRef = collection(db, collectionName);
   return await addDoc(colRef, data);
+}
+
+export async function deleteData(collectionName: string, id: string) {
+  const colRef = collection(db, collectionName);
+  return await deleteDoc(doc(colRef, id));
 }
