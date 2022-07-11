f3a488bd1d95a1fd1cd242cad53ae1fc52912cec
dmitry <dmitryspiridonov56@gmail.com>
2022-07-11 17:37:58 +0300

    implement tweet delete

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -3,9 +3,10 @@ import Trends from '@Components/trends/Trends';
 import { useAppDispatch, useAppSelector } from '@Hooks/redux';
 import { Tweet } from '@Models/tweet';
 import { setTweets } from '@Store/reducers/TweetSlice';
-import { addData, getData } from '@Utils/firestore-methods';
+import { addData, deleteData, getData } from '@Utils/firestore-methods';
 import { Timestamp } from 'firebase/firestore';
 import { FunctionComponent, useCallback, useEffect, useState } from 'react';
+import { CardEvent } from './models/enums';
 import Card from './card/Card';
 import HomeHeader from './home-header/Home-header';
 
@@ -33,6 +34,18 @@ const Home: FunctionComponent = () => {
     }
   };
 
+  const cardEvent = (cardEvent: CardEvent, data: Tweet) => {
+    switch (cardEvent) {
+      case CardEvent.Delete:
+        deleteData(colName, data.id).then(() => getTweets());
+        break;
+
+      default:
+        console.log('There is no such event');
+        break;
+    }
+  };
+
   useEffect(() => {
     getTweets();
   }, [getTweets]);
@@ -46,7 +59,12 @@ const Home: FunctionComponent = () => {
           <div>Loading...</div>
         ) : (
           tweets.map((tweet, index) => (
-            <Card tweet={tweet} key={tweet.id} isFirst={index === 0} />
+            <Card
+              tweet={tweet}
+              key={tweet.id}
+              isFirst={index === 0}
+              onEvent={(eventType, data) => cardEvent(eventType, data)}
+            />
           ))
         )}
       </div>
--- src/views/home/card/Card.tsx
+++ src/views/home/card/Card.tsx
@@ -1,23 +1,54 @@
-import { FunctionComponent } from 'react';
+import ConfirmDialog from '@Components/confirm-dialog/Confirm-dialog';
+import { FunctionComponent, useState } from 'react';
 import { Tweet } from '../../../models/tweet';
+import { CardEvent } from '../models/enums';
+import TweetMenu from '../tweet-menu/TweetMenu';
 import styles from './Card.module.scss';
 
 interface TweetCardProps {
   tweet: Tweet;
   isFirst: boolean;
+  onEvent: (eventType: CardEvent, tweet: Tweet) => void;
 }
 
 const TweetCard: FunctionComponent<TweetCardProps> = ({
   tweet,
   isFirst,
-}: TweetCardProps) => (
-  <div className={`${styles.card} ${isFirst ? styles.first : ''}`}>
-    <h3>{tweet.name}</h3>
-    <p className={styles.card__text}>{tweet.text}</p>
-    <span>{`${tweet.date.toDate().toDateString()} ${tweet.date
-      .toDate()
-      .toLocaleTimeString()}`}</span>
-  </div>
-);
+  onEvent,
+}: TweetCardProps) => {
+  const [isOpen, setOpen] = useState(false);
+  const handleOption = (option: string) => {
+    switch (option) {
+      case CardEvent.Delete:
+        setOpen(true);
+        break;
+      default:
+        break;
+    }
+  };
+  return (
+    <>
+      <div className={`${styles.card} ${isFirst ? styles.first : ''}`}>
+        <h3>{tweet.name}</h3>
+        <p className={styles.card__text}>{tweet.text}</p>
+        <span>{`${tweet.date.toDate().toDateString()} ${tweet.date
+          .toDate()
+          .toLocaleTimeString()}`}</span>
+        <div className={styles.card__menu}>
+          <TweetMenu onOption={handleOption} />
+        </div>
+      </div>
+      <ConfirmDialog
+        titleText="Удалить твит?"
+        isOpen={isOpen}
+        onConfirm={() => {
+          onEvent(CardEvent.Delete, tweet);
+          setOpen(false);
+        }}
+        onReject={() => setOpen(false)}
+      />
+    </>
+  );
+};
 
 export default TweetCard;
